9e8078d0b01b392813a9354796872dd193c66df8
NextKolya <phelixgluy@gmail.com>
2025-12-20 13:25:40 +0600

    Add RouteHandler type in route types. Add docs for route types

--- src/types/route.ts
+++ src/types/route.ts
@@ -1,5 +1,13 @@
 // TODO: add docs
 
+/**
+ * HTTP Method primitive.
+ *
+ * @example
+ * ```typescript
+ * const method: HttpMethod = 'GET';
+ * ```
+ */
 export type HttpMethod =
     | 'GET'
     | 'POST'
@@ -8,9 +16,11 @@ export type HttpMethod =
     | 'DELETE'
     | 'OPTIONS';
 
+/**
+ * HTTP Header struct.
+ */
 export type Header = {
     name: string;
-
     value: string;
 };
 export type Headers = ResponseInit['headers'];
@@ -29,18 +39,23 @@ export interface RouteResponse<
     T extends { body: unknown } = { body: unknown }
 > {
     setHeader: (name: Header['name'], value: Header['value']) => void;
-
     send: (data: T['body'], options: ResponseOptions) => void;
 }
 
 export type Route = Partial<Record<HttpMethod, RouteOptions>>;
+
+export type RouteHandler = (
+    request: RouteRequest,
+    response: RouteResponse
+) => void;
+
 export type RouteOptions = {
     url: string;
     method: HttpMethod;
 
-    onRequest?: (request: RouteRequest, response: RouteResponse) => void;
+    onRequest?: RouteHandler;
 
-    preHandler?: (request: RouteRequest, response: RouteResponse) => void;
+    preHandler?: RouteHandler;
 
-    handler: (request: RouteRequest, response: RouteResponse) => void;
+    handler: RouteHandler;
 };
